a0fd096d62e0eb7ae0142e98a54b7a40f07b7a36
thoriq erptohama <thoriq@erptohama.co.jp>
2025-04-29 17:03:16 +0700

    feat: adding starter code

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,23 @@
-"use client";
-
-// pages/index.js
-import Head from 'next/head';
-import Link from 'next/link';
-
 export default function Home() {
+  // TODO: Add state for counter
+  
   return (
-    <div>
-      <h1>Gabrielle</h1>
-    </div>
-  );
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Next.js App Router Demo</h1>
+      
+      {/* TODO: Demo React Fragments */}
+      
+      {/* TODO: Display counter state */}
+      
+      {/* TODO: Add button to update state */}
+      
+      {/* TODO: Use a reusable component with props */}
+    </main>
+  )
 }
+
+// File: src/components/Counter.js
+// TODO: Create a reusable Counter component
+
+// File: src/components/Card.js
+// TODO: Create a reusable Card component
